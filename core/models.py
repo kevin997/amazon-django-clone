175b09c0268b8fc6270b12f9fb63aa2eaab78ee7
Ovanga Liboire Kevin <kevinliboire@gmail.com>
2023-06-04 17:13:53 +0100

    added fixes

---
+++ core/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django_extensions.db.models import (
+	TimeStampedModel, 
+	ActivatorModel,
+	TitleDescriptionModel
+)
+
+class Contact(
+	TimeStampedModel, 
+	ActivatorModel,
+	TitleDescriptionModel,
+	):
+
+	class Meta:
+		verbose_name_plural = "Contacts"
+
+	email = models.EmailField(verbose_name="Email")
+
+	def __str__(self):
+		return f'{self.title}'
